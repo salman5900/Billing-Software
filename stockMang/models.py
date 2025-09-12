68a20a4d4eb2498df981bf2c294f463233cc7282
salman5900 <salmanahamed590@gmail.com>
2025-09-12 15:14:32 +0000

    Change made to view

--- stockMang/models.py
+++ stockMang/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    hsn_code = models.CharField(max_length=8, blank=True, null=True)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    # Store tax rate as a percentage, e.g., 18 for 18%
+    tax_rate = models.DecimalField(max_digits=4, decimal_places=2, default=18.00) 
+    # This is the taxable value / base price
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+
--- stockMang/views.py
+++ stockMang/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 def stock(request):
-    return render(request, 'stockMang/stockPage.html')
+    Product = Product.objects.all().order_by('-created_at')
+    return render(request, 'stockMang/stockPage.html', {'products': Product})
