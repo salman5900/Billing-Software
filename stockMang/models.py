3b80922ba70556469d8e3ef07cb22dd9108b6725
salman5900 <salmanahamed590@gmail.com>
2025-09-12 15:16:29 +0000

    time zone added

--- stockMang/models.py
+++ stockMang/models.py
@@ -10,7 +10,8 @@ class Product(models.Model):
     tax_rate = models.DecimalField(max_digits=4, decimal_places=2, default=18.00) 
     # This is the taxable value / base price
     selling_price = models.DecimalField(max_digits=10, decimal_places=2)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
 
